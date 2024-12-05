9510840613c4666023f7f3906b804f63da95f066
SimonIvarsson <simon.ivarsson@elev.ga.ntig.se>
2024-12-05 08:57:29 +0100

    Början av spelet

---
+++ index.js
@@ -0,0 +1,16 @@
+// Hämtar texten för poängen för spelare 1 | Simon I
+const currentText = document.getElementById("current-1");
+
+// När knappen trycks så slumpas ett tal mellan 1-6  och ändrar poängen | Simon I
+const roll_dice = document.getElementById("roll-dice");
+let totalPoints = 1;
+roll_dice.addEventListener("click", function(){
+    var points = (Math.floor(Math.random() * 6) + 1);
+    if (points === 1) {
+        totalPoints = 0;
+    } 
+    else {
+        totalPoints += points; 
+    }
+    currentText.textContent = "Current: " + totalPoints;
+});
